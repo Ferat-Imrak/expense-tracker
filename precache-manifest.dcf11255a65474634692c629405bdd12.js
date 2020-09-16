d355ea84cd969bd52e9015d056efc4a6b6d59f02
Ferhat <ferhat@MacBook-Pro.local>
2020-09-15 22:34:14 -0500

    Updates

---
+++ precache-manifest.dcf11255a65474634692c629405bdd12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a897281e0e2ad82696106d5b1bd182a",
+    "url": "/jiro1/expense-tracker/index.html"
+  },
+  {
+    "revision": "bdd6f24c9803aeb0c33f",
+    "url": "/jiro1/expense-tracker/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "aefcadf35b12fe9f2208",
+    "url": "/jiro1/expense-tracker/static/js/2.e85e92f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jiro1/expense-tracker/static/js/2.e85e92f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd6f24c9803aeb0c33f",
+    "url": "/jiro1/expense-tracker/static/js/main.2bd25893.chunk.js"
+  },
+  {
+    "revision": "441b1d2ce23efcef15a0",
+    "url": "/jiro1/expense-tracker/static/js/runtime-main.51ac5cbe.js"
+  }
+]);
